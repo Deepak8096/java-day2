2e99a6add320b4a295dd8760a49e2aad318cc444
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-03 10:30:43 +0530

    Add files via upload

---
+++ comp.java
@@ -0,0 +1,11 @@
+public class comp {
+    public static void main(String[] args) {
+        int a=23;
+        int b=25;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }
+    
+}
