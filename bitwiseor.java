ada8aa7a1b937f4e7547d02d6851e866ad240bf5
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-03 12:41:11 +0530

    Add files via upload

---
+++ bitwiseor.java
@@ -0,0 +1,8 @@
+public class bitwiseor {
+    public static void main(String[] args) {
+        System.out.println(11|18);
+        System.out.println(22|144);
+        System.out.println(88|122);
+    }
+    
+}
