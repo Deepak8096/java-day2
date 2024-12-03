6a2ddcaee5b391035b05c3793e1eb4724176990c
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-03 11:29:18 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class swap {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int g=sc.nextInt();
+        int b=sc.nextInt();
+
+        int c=b;
+        b=g;
+        g=c;
+        String res=String.format("after swapping\nboy=%d\ngirl=%d",g,b);
+        System.out.println(res);
+
+    }
+    
+}
